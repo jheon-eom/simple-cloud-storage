d1d7a03945d58a1b10e38189e1f41e411822a7fc
eomjongheon <e4033jh@gmail.com>
2025-01-22 17:45:48 +0900

    Project init

---
+++ src/main/java/com/eomproject/simple_storage/SimpleStorageApplication.java
@@ -0,0 +1,12 @@
+package com.eomproject.simple_storage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleStorageApplication.class, args);
+	}
+}
---
+++ src/test/java/com/eomproject/simple_storage/SimpleStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eomproject.simple_storage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
