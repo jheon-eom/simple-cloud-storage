41bef63abaca5d282c006839ea9a8821e0ed52d7
eomjongheon <e4033jh@gmail.com>
2025-01-24 15:16:27 +0900

    #250124-3
    - AuditingEntity 추가

---
+++ src/main/java/com/eomproject/simple_storage/common/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.eomproject.simple_storage.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaAuditingConfig {
+}
--- src/main/java/com/eomproject/simple_storage/common/entity/AuditingEntity.java
+++ src/main/java/com/eomproject/simple_storage/common/entity/AuditingEntity.java
@@ -1,13 +1,22 @@
 package com.eomproject.simple_storage.common.entity;
 
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
+@Getter
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class AuditingEntity {
 
+    @CreatedDate
     private LocalDateTime createdAt;
 
+    @LastModifiedDate
     private LocalDateTime updatedAt;
 }
