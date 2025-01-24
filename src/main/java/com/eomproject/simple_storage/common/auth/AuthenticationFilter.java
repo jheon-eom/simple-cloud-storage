ca2e88f6486def665853df35d265c497d85531aa
eomjongheon <e4033jh@gmail.com>
2025-01-24 17:59:02 +0900

    #250124-15
    - 필터 구현 및 테스트 진행 중
    - @AutoConfigureMockMvc, @SpringBootTest 어노테이션 동작 원리 파악해야 함

--- src/main/java/com/eomproject/simple_storage/common/auth/AuthenticationFilter.java
+++ src/main/java/com/eomproject/simple_storage/common/auth/AuthenticationFilter.java
@@ -1,9 +1,12 @@
 package com.eomproject.simple_storage.common.auth;
 
 import jakarta.servlet.*;
+import org.springframework.stereotype.Component;
 
+import javax.security.sasl.AuthenticationException;
 import java.io.IOException;
 
+@Component
 public class AuthenticationFilter implements Filter {
 
     @Override
@@ -11,7 +14,7 @@ public class AuthenticationFilter implements Filter {
             throws IOException, ServletException {
 
         if (servletRequest.getAttribute("userId") == null) {
-            throw new ServletException("Unauthorized access");
+            throw new AuthenticationException("인증되지 않은 사용자입니다.");
         }
     }
 }
--- src/test/java/com/eomproject/simple_storage/common/auth/AuthenticationFilterTest.java
+++ src/test/java/com/eomproject/simple_storage/common/auth/AuthenticationFilterTest.java
@@ -2,6 +2,7 @@ package com.eomproject.simple_storage.common.auth;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -9,6 +10,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@AutoConfigureMockMvc
 @SpringBootTest
 class AuthenticationFilterTest {
 
@@ -17,8 +19,7 @@ class AuthenticationFilterTest {
 
     @Test
     void 화이트_리스트가_아닌_경우_인증_헤더를_검사하여_헤더가_없을_경우_예외가_발생한다() throws Exception {
-        // given
+        // when
         mockMvc.perform(get("/api/v1/some-api")).andExpect(status().isUnauthorized());
-        System.out.println();
     }
 }
