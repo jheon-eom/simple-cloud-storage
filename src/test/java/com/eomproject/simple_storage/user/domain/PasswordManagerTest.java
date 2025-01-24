05b5dc0a66c0166c4d3884e820390f900cc55f79
eomjongheon <e4033jh@gmail.com>
2025-01-24 16:22:02 +0900

    #250124-11
    - 패스워드 암호화 테스트 코드 추가

---
+++ src/test/java/com/eomproject/simple_storage/user/domain/PasswordManagerTest.java
@@ -0,0 +1,25 @@
+package com.eomproject.simple_storage.user.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PasswordManagerTest {
+
+    private final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    @Test
+    void 패스워드_암호화_테스트() {
+        // given
+        PasswordManager passwordManager = new PasswordManager(passwordEncoder);
+        String password = "1234";
+
+        // when
+        String encryptedPassword = passwordManager.encryptPassword(password);
+
+        // then
+        assertNotEquals(password, encryptedPassword);
+    }
+}
