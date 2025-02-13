f4210c3079d3649262f086d2409b7afe71f163e6
eomjongheon <e4033jh@gmail.com>
2025-02-13 16:47:07 +0900

    #250213-2
    - 회원가입 테스트 목 객체 생성 후 주입

--- src/test/java/com/eomproject/simple_storage/user/application/RegisterUserServiceTest.java
+++ src/test/java/com/eomproject/simple_storage/user/application/RegisterUserServiceTest.java
@@ -1,10 +1,9 @@
 package com.eomproject.simple_storage.user.application;
 
-import com.eomproject.simple_storage.file.adapter.out.filesystem.CreateDirectoryFileSystemAdapter;
-import com.eomproject.simple_storage.file.adapter.out.persistance.SaveDirectoryJpaAdapter;
 import com.eomproject.simple_storage.file.application.CreateDirectoryService;
-import com.eomproject.simple_storage.file.application.FilenameProvider;
 import com.eomproject.simple_storage.file.application.port.in.CreateDirectoryUseCase;
+import com.eomproject.simple_storage.file.application.port.out.CreateDirectoryPort;
+import com.eomproject.simple_storage.file.application.port.out.SaveDirectoryPort;
 import com.eomproject.simple_storage.file.mock.TestFilenameProvider;
 import com.eomproject.simple_storage.user.application.dto.RegisterUserCommand;
 import com.eomproject.simple_storage.user.application.port.out.RegisterUserPort;
@@ -21,18 +20,27 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class RegisterUserServiceTest {
 
+    // DI
     @Autowired
     PasswordManager passwordManager;
     @Autowired
     RegisterUserPort registerUserPort;
     @Autowired
-    CreateDirectoryUseCase createDirectoryUseCase;
+    CreateDirectoryPort createDirectoryPort;
+    @Autowired
+    SaveDirectoryPort saveDirectoryPort;
+
+    // Mock
+    TestFilenameProvider testFilenameProvider = new TestFilenameProvider();
 
     @Test
     void 회원가입이_성공하면_유저_데이터와_루트_디렉토리_메타데이터가_디비에_저장되고_루트_디렉토리가_생성된다() {
         // given
+        CreateDirectoryUseCase testCreateDirectoryUseCase =
+                new CreateDirectoryService(testFilenameProvider, createDirectoryPort, saveDirectoryPort);
+
         RegisterUserService registerUserService =
-                new RegisterUserService(passwordManager, registerUserPort, createDirectoryUseCase);
+                new RegisterUserService(passwordManager, registerUserPort, testCreateDirectoryUseCase);
         RegisterUserCommand command = new RegisterUserCommand("test1234", "test12345");
 
         // when
